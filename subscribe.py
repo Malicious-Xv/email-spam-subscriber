029a875d83b74f1947266192e9ed2bb6637530da
XtramCZ <69092797+XtramCZ@users.noreply.github.com>
2021-08-13 19:54:06 +0200

    Update subscribe.py

--- subscribe.py
+++ subscribe.py
@@ -13,16 +13,12 @@ def load():
     print("█████╗█████╗█████╗█████╗█████╗█████╗")
     print("╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝")
     print()
-    print()
-    print()
     print("      ███████╗███████╗███████╗      ")
     print("      ██╔════╝██╔════╝██╔════╝      ")
     print("█████╗█████╗  ███████╗███████╗█████╗")
     print("╚════╝██╔══╝  ╚════██║╚════██║╚════╝")
     print("      ███████╗███████║███████║      ")
     print("      ╚══════╝╚══════╝╚══════╝      ")
-    print()
-    print()
     print()
     print("█████╗█████╗█████╗█████╗█████╗█████╗")
     print("╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝")
@@ -54,21 +50,21 @@ for email in emails:
         try:
             res1 = requests.post('https://www.biblegateway.com/newsletters/subscribe/', allow_redirects=False, data=data1)
             print(Fore.GREEN + '> ' + Style.RESET_ALL + 'Sucessfully subscribed ' + Fore.CYAN + '{} '.format(email) + Style.RESET_ALL + 'to ' + Fore.CYAN + 'www.biblegateway.com')
-            time.sleep(0.3)
+            time.sleep(0.2)
         except Exception as e:
             error(e, "biblegateway.com")
 
         try:
             res2 = requests.post('https://www.nbc26.com/account/manage-email-preferences', allow_redirects=False, data=data2)
             print(Fore.GREEN + '> ' + Style.RESET_ALL + 'Sucessfully subscribed ' + Fore.CYAN + '{} '.format(email) + Style.RESET_ALL + 'to ' + Fore.CYAN + 'www.nbc26.com')
-            time.sleep(0.3)
+            time.sleep(0.2)
         except Exception as e:
             error(e, "nbc26.com")
 
         try:
             res3 = requests.post('https://api.ewscloud.com/prod/notifications/v1/wgba/contactlists/subscribe/', allow_redirects=False, data=data3)
             print(Fore.GREEN + '> ' + Style.RESET_ALL + 'Sucessfully subscribed ' + Fore.CYAN + '{} '.format(email) + Style.RESET_ALL + 'to ' + Fore.CYAN + 'api.ewscloud.com')
-            time.sleep(0.3)
+            time.sleep(0.2)
         except Exception as e:
             error(e, 'ewscloud.com')
     except KeyboardInterrupt:
